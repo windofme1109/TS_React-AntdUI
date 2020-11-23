0732565d8030fa5c0045455c4ccf69c62ff2719b
qinmr <qinmr@yonyou.com>
2020-11-23 13:02:15 +0800

    添加 UploadList 组件

---
+++ viking-ship/src/components/Upload/uploadList.tsx
@@ -0,0 +1,14 @@
+import React, {FC, Fragment} from 'react';
+
+const UploadList: FC = (props) => {
+    return (
+        <Fragment>
+            <ul className="viking-upload=list">
+
+            </ul>
+        </Fragment>
+    )
+
+}
+
+export default UploadList;
