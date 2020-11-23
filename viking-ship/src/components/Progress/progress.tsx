f24ff9daa410a8d47394bf8ead5baf23b8f5b1fe
qinmr <qinmr@yonyou.com>
2020-11-23 20:58:10 +0800

    添加 Progress (进度条) 组件

---
+++ viking-ship/src/components/Progress/progress.tsx
@@ -0,0 +1,14 @@
+import React, {FC, Fragment} from 'react';
+
+const Progress: FC<> = (props) => {
+    return (
+        <Fragment>
+            <div className="viking-progress-bar">
+
+            </div>
+        </Fragment>
+    )
+
+}
+
+export default Progress;
